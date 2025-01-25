af9af59fc359b7ed4329f4f2c194f568117ecf14
Williams Joseph <wj882244@gmail.com>
2025-01-25 15:30:15 +0100

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,6 +28,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="nD29tshy6v_FR2eopYjdeY36S65XJccsKVnFa_0YPYY"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
